aada0c37d280bc6bc34b8bffedcb12824c711656
David Serber <david@onthemargin.app>
2020-02-28 18:25:11 -0600

    Played aroudn with popup page and search page.

--- public/js/app.js
+++ public/js/app.js
@@ -40,3 +40,14 @@ if (document.querySelector('.view-main')) {
   var mainView = app.views.create('.view-main');
 }
 
+// create searchbar
+var searchbar = app.searchbar.create({
+  el: '.searchbar',
+  searchContainer: '.list',
+  searchIn: '.item-title',
+  on: {
+    search(sb, query, previousQuery) {
+      console.log(query, previousQuery);
+    }
+  }
+});
